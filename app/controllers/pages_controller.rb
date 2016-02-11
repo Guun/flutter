abee91d58f3bb48a0da39e0939b3c565e43cd219
guun1 <stevend173@hotmail.com>
2016-02-11 16:24:15 +0000

    Createed Pages controller. Added explore, home, index and profile pages.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  
+  #back-end code for pages/index
+  def index
+  end
+
+  #back-end code for pages/index
+  def home
+  end
+
+  #back-end code for pages/index
+  def profile
+  end
+
+  #back-end code for pages/index
+  def explore
+  end
+end
