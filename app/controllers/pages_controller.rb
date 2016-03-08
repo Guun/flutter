173833fa3ec71740ef8872831a07fa12fc0d89a0
guun1 <stevend173@hotmail.com>
2016-03-08 19:23:49 +0000

    On flutter1. Had hiccup with db on flutter. Vid 9 done, relationships and follower handling in place

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,32 +1,42 @@
+# The pages controller contains all of the code for any page inside of /pages
 class PagesController < ApplicationController
   
-  #back-end code for pages/index
+  # back-end code for pages/index
   def index
   end
 
-  #back-end code for pages/home
+  # back-end code for pages/home
   def home
-    @posts = Post.all
+    following = Array.new
+    for @f in current_user.following do
+      following.push(@f.id)
+    end
+
+    @posts = Post.where("user_id IN (?)", following)
     @newPost = Post.new
   end
 
-  #back-end code for pages/profile
+  # back-end code for pages/profile
   def profile
-    #grab the username from the URL as :id
-    if(User.find_by_username(params[:id]))
+    # grab the username from the URL as :id
+    if (User.find_by_username(params[:id]))
       @username = params[:id]
-    else
-      #redirect to 404 (root for now)
-      redirect_to root_path, :notice=> "User not found!"
+    else 
+      # redirect to 404 (root for now)
+      redirect_to root_path, :notice=> "User not found!" 
     end
     
-    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)  #get posts from the post table in the database
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
     @newPost = Post.new
+    
+    @toFollow = User.all.last(5)
   end
 
-  #back-end code for pages/explore
+  # back-end code for pages/explore
   def explore
     @posts = Post.all
     @newPost = Post.new
+        @toFollow = User.all.last(5)
   end
+  
 end
